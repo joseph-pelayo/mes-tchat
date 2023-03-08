2b5e0b69834f1b87a471ae7f37aa2bfdafa2edcf
FidelMH <moussahaziri.fidel@gmail.com>
2023-03-08 16:10:22 +0400

    added chat_ajax.php

--- mod/chat/chat_header.php
+++ mod/chat/chat_header.php
@@ -1,21 +1,7 @@
 <?php
 
-// Ajout d'un message
-// Verifier si on a cliqué sur le bouton envoyer
-if(isset($_POST) && !empty($_POST)){
-    // L'utilisateur a cliqué sur envoyer => Analyser le retour
-    $h_message = array();
-    $h_message['message'] = $_POST['form_nom'];
-    $h_message['date'] = date("Y-m-d H:i:s");
-    $h_message['user_id'] = $_SESSION[SITE_NAME]['id_user'];
-    if(!empty($_POST['form_password']))
-        $h['password'] = md5($_POST['form_password']);
-    $h['adresse'] = $_POST['form_adresse'];
-    $h['code_postal'] = $_POST['form_cp'];
-    $h['fk_ville'] = $_POST['form_ville'];
-    $h['fk_pays'] = $_POST['form_pays'];
 
-}
+
 // Récupération des messages et pièces jointe associés
 
     $title_page = "Chat";
