6aa9f1ce62610381559af86b5a3d8c679f00ea41
FidelMH <moussahaziri.fidel@gmail.com>
2023-03-08 15:01:55 +0400

    back end listing message

---
+++ mod/chat/chat_header.php
@@ -0,0 +1,62 @@
+<?php
+
+    // Etape 1 : Ecriture de la requete SQL
+    // $sql = "SELECT 
+    //                 p.id, 
+    //                 p.data AS data,
+    //                 t.libelle as type_piece,
+    //                 p.message_id as message_id,
+    //                 u.login as user
+    //         FROM piece as p
+    //         LEFT JOIN type_piece as t ON p.type_id = t.id
+    //         LEFT JOIN message as m ON p.type_id = m.id
+    //         LEFT JOIN t_user as u ON m.user_id = u.login;";
+
+    $title_page = "Chat";
+    $html ="";
+    
+    $datas = [] ;
+    $sql = "SELECT * FROM message;";
+    $res = query($sql);
+    
+    if($res && mysqli_num_rows($res)){
+        
+        while($data = mysqli_fetch_assoc($res)){
+            $message= array('id' =>$data['id'], 'text' =>  $data['message'], 'date'=> $data['date']);
+            // $message['id'] = $data['id'];
+            // $message['text'] = $data['message'];
+            // $message['date'] = $data['date'];
+            $user_id = $data['user_id'];
+            $message_id = $data['id'];
+        }
+        $sql = "SELECT login FROM t_user WHERE id =".$user_id;
+        $user = squery($sql);
+
+        $sql = "SELECT  p.id, 
+                        p.data AS data, 
+                        t.libelle as type
+                from piece as p
+                LEFT JOIN type_piece as t ON p.type_id = t.id
+                WHERE message_id=". $message_id ;
+        $res = query($sql);
+        $pieces=[];
+        if($res && mysqli_num_rows($res)){
+            
+            while($data_piece = mysqli_fetch_assoc($res)){
+                $pieces[]= array('id'=>$data_piece['id'], 'data' => $data_piece['data'],'type' =>$data_piece['type'] );
+            }
+
+        }
+        $datas[] = array('user' => $user, 'message' => $message, 'pieces'=> $pieces);
+    }
+
+    // var_dump($datas);
+    $html.= '<p>'.$datas[0]['user'].'</p>'.PHP_EOL; // 
+    // echo($datas[0]['message']).PHP_EOL;  // 
+    $html.= '<p>'.$datas[0]['message']['id'].'</p>'.PHP_EOL; // 
+    $html.= '<p>'.$datas[0]['message']['text'].'</p>'.PHP_EOL; // 
+    $html.= '<p>'.$datas[0]['message']['date'].'</p>'.PHP_EOL; // 
+    // echo ($datas[0]['pieces']).PHP_EOL; // liste pièce jointe
+    $html.= '<p>'.$datas[0]['pieces'][0]['data'].'</p>'.PHP_EOL; 
+    $html.= '<p>'.$datas[0]['pieces'][0]['type'].'</p>'.PHP_EOL; 
+?>
--- param/route.php
+++ param/route.php
@@ -4,5 +4,6 @@
     $page['galerie'] = 'mod/galerie/galerie.php';
     $page['home'] = 'mod/home/home.php';
     $page['mosaic'] = 'mod/mosaic/mosaic.php';
+    $page['chat'] = 'mod/chat/chat.php';
 
 ?>
